220e8051760ac57d8f7c44dd5d930d3bd5b5dd1c
Vijay Pathak <helloblazevijay99@gmail.com>
2019-12-18 19:39:40 +0545

    update package and fix name in properties

--- scripts/tabular_data.py
+++ scripts/tabular_data.py
@@ -1,98 +1,46 @@
 datapackage = '''{
-    "profile": "tabular-data-package",
-    "name": "Natural Gas prices",
+    "name": "natural-gas-prices",
     "title": "Henry Hub Natural Gas Spot prices at https://www.eia.gov/dnav/ng/hist/rngwhhdm.htm",
     "resources": [
         {
+            "name": "price_daily",
             "path": "data/price_daily.csv",
             "format": "csv",
             "mediatype": "text/csv",
-            "name": "daily",
             "schema": {
                 "fields": [
                     {
                         "format": "default",
-                        "name": "Date",
+                        "name": "date",
                         "type": "date"
                     },
                     {
                         "format": "default",
-                        "name": "Price",
+                        "name": "price",
                         "type": "number"
                     }
-                ],
-        "missingValues": [
-          ""
-        ]
+                ]
             }
         },
         {
+            "name": "price_weekly.csv",
             "path": "data/price_weekly.csv",
             "format": "csv",
             "mediatype": "text/csv",
-            "name": "daily",
             "schema": {
                 "fields": [
                     {
                         "format": "default",
-                        "name": "Week",
+
+                        "name": "date",
                         "type": "date"
                     },
                     {
                         "format": "default",
-                        "name": "Price",
+                        "name": "price",
                         "type": "number"
                     }
-                ],
-        "missingValues": [
-          ""
-        ]
-            }
-        },
-        {
-            "path": "data/price_montly.csv",
-            "format": "csv",
-            "mediatype": "text/csv",
-            "name": "montly",
-            "schema": {
-                "fields": [
-                    {
-                        "format": "default",
-                        "name": "Date",
-                        "type": "date"
-                    },
-                    {
-                        "format": "default",
-                        "name": "Price",
-                        "type": "number"
-                    }
-                ],
-        "missingValues": [
-          ""
-        ]
-            }
-        },
-        {
-            "path": "data/price_annual.csv",
-            "format": "csv",
-            "mediatype": "text/csv",
-            "name": "daily",
-            "schema": {
-                "fields": [
-                    {
-                        "format": "default",
-                        "name": "Year",
-                        "type": "date"
-                    },
-                    {
-                        "format": "default",
-                        "name": "Price",
-                        "type": "number"
-                    }
-                ],
-        "missingValues": [
-          ""
-        ]
+                ]
             }
         }
     ]
